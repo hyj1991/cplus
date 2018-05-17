ec9c370f01cfef3f42fe4ec380fda44acf6eb91e
hyj1991 <66cfat66@gmail.com>
2018-05-17 18:50:12 +0800

    add: chapter 2 files
      - getinfo.cpp
      - myfirst.cpp
      - sqrt.cpp

---
+++ 0x0002_第二章_开始学习C++/myfirst.cpp
@@ -0,0 +1,12 @@
+//  myfirst.cpp -- dispalys a message
+#include <iostream>
+
+int main(void) {
+  using namespace std;
+  cout << "Come up and c++ me some time.";
+  cout << endl;
+  cout << "You won't regret it!" << endl;
+  // 控制窗口一直打开
+  cin.get();
+  return 0;
+}
